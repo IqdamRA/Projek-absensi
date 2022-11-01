db903b7a44898cb840f934e0a1d6b11586adfb30
melanycantik <arummailany16@gmail.com>
2022-11-01 11:02:09 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'src/navigationControl.dart';
+import 'src/webview_stack.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter WebView'),
+          actions: [
+            NavigationControls(controller: controller),
+          ],
+        ),
+        body: WebViewStack(controller: controller));
+  }
+}
